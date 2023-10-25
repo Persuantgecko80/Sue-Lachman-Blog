6adf3542e8cff934b5b2c24025b557b7a48b2a93
Yosef Lachman <46474203+Persuantgecko80@users.noreply.github.com>
2023-10-25 17:57:30 +0300

    started to work on the pop up modals

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // Smooth scroll
     const link = document.querySelector(".to-bottom");
     const target = document.getElementById("bottom");
   
     link.addEventListener("click", function(event) {
       event.preventDefault();
-      
       target.scrollIntoView({ behavior: "smooth" });
     });
+  
+    // Modal
+    var modal = document.getElementById("myModal");
+    var card = document.getElementById("special-card-1");
+
+
+    var span = document.getElementsByClassName("close")[0];
+  
+    card.onclick = function() {
+      modal.style.display = "block";
+    }
+  
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+  
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
   });
   
