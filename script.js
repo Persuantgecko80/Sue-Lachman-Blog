e816375e0b2f47bae4cd3bbcd9ac9433d8b70557
Yosef Lachman <46474203+Persuantgecko80@users.noreply.github.com>
2023-10-25 19:11:06 +0300

    tweaks with JS to get the modals to open and close nicely

--- script.js
+++ script.js
@@ -8,30 +8,36 @@ document.addEventListener("DOMContentLoaded", function() {
       target.scrollIntoView({ behavior: "smooth" });
     });
 
-    // Handle multiple modals
-    let openModal = null;  // Keep track of the currently open modal
+    let openModal = null; // Keep track of the currently open modal
 
+    // Handle multiple modals
     for (let i = 1; i <= 5; i++) {
         let modal = document.getElementById(`myModal-${i}`);
         let card = document.getElementById(`special-card-${i}`);
         let span = modal.querySelector(".close");
-        
+
         card.onclick = function() {
-          modal.style.display = "block";
-          openModal = modal;  // Update the currently open modal
+            modal.style.opacity = 0;
+            modal.style.display = "block";
+            setTimeout(() => modal.style.opacity = 1, 1);
+            openModal = modal; // Store the opened modal
         }
 
         span.onclick = function() {
-          modal.style.display = "none";
-          openModal = null;  // Reset the open modal tracker
+            modal.style.opacity = 0;
+            setTimeout(() => modal.style.display = "none", 300);
+            openModal = null; // Reset the opened modal
         }
     }
 
-    // Close the modal when clicking outside of it
+    // Single window click listener
     window.onclick = function(event) {
-      if (event.target === openModal) {
-        openModal.style.display = "none";
-        openModal = null;  // Reset the open modal tracker
-      }
+        if (openModal && event.target == openModal) {
+            openModal.style.opacity = 0;
+            setTimeout(() => {
+                openModal.style.display = "none";
+                openModal = null; // Reset the opened modal
+            }, 300);
+        }
     }
 });
